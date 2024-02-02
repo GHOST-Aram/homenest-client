2a579175b62aa1876952b14894530584682a199b
GHOST-Aram <owinoojala@gmail.com>
2024-02-02 14:30:13 +0300

    refactor: Change text styles
             - Change text color styles on FeaturedCard component

--- src/components/FeaturedCard.tsx
+++ src/components/FeaturedCard.tsx
@@ -13,9 +13,15 @@ const FeaturedCard = ({ rentPm, location, bedrooms, imageSrc }: FeaturedCardProp
                 />
             </div>
             <div className="bg-slate-400 px-4 w-full rounded-b-md">
-                <p>Rent per Month: <span className='font-bold'>{rentPm}</span> </p>
-                <p>Location: <span className='font-bold'>{location}</span> </p>
-                <p>Bedrooms: <span className='font-bold'>{bedrooms}</span> </p>
+                <p className='space-x-2'> 
+                    <span className='font-bold text-slate-700'>Rent per Month:</span> 
+                    <span className='font-bold'>{rentPm}</span> </p>
+                <p className='space-x-2'> 
+                    <span className='font-bold text-slate-700'>Location:</span> 
+                    <span className='font-bold'>{location}</span> </p>
+                <p className='space-x-2'> 
+                    <span className='font-bold text-slate-700'>Bedrooms:</span> 
+                    <span className='font-bold'>{bedrooms}</span> </p>
                 <div className="text-center py-4">
                     <ButtonFilled className='w-full'>READ MORE</ButtonFilled>
                 </div>
