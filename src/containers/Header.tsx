d6242c659769528d99cc2a215ba348b44f572831
GHOST-Aram <owinoojala@gmail.com>
2024-02-02 07:20:06 +0300

    chore: Change logo
           - Replace the blue logo with a white logo

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Logo from '../components/Logo'
-import logo from '../images/homenest-logo.png'
+import logo from '../images/homenest-logo-bw.png'
 import SearchButton from '../components/SearchButton';
 import MenuButton from '../components/MenuButton';
 import SearchBox from '../components/SearchBox';
