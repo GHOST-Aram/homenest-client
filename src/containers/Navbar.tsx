9e77b4762a68a576232a64edf50e9adfff7fe355
GHOST-Aram <owinoojala@gmail.com>
2024-02-02 07:24:50 +0300

    improvement: Style active navigation link
                - Add styles to '.active' css class

--- src/containers/Navbar.tsx
+++ src/containers/Navbar.tsx
@@ -3,20 +3,22 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <nav className='flex-row gap-4 items-center hidden md:flex lg:flex xl:flex'>
-            <NavLink to={'/'} className={'p-4 text-slate-300'}>
+        <nav className='hidden flex-row gap-4 items-center md:justify-center 
+            md:flex lg:flex xl:flex'
+        >
+            <NavLink to={'/'} className={'p-4 text-slate-300 font-bold'}>
                 HOME
             </NavLink>
-            <NavLink to={'/listings'} className={'p-4 text-slate-300'}>
+            <NavLink to={'/listings'} className={'p-4 text-slate-300 font-bold'}>
                 LISTINGS
             </NavLink>
-            <NavLink to={'/how-it-works'} className={'p-4 text-slate-300'}>
+            <NavLink to={'/how-it-works'} className={'p-4 text-slate-300 font-bold'}>
                 HOW IT WORKS
             </NavLink>
-            <NavLink to={'/about-us'} className={'p-4 text-slate-300'}>
+            <NavLink to={'/about-us'} className={'p-4 text-slate-300 font-bold'}>
                 ABOUT US
             </NavLink>
-            <NavLink to={'/contact-us'} className={'p-4 text-slate-300'}>
+            <NavLink to={'/contact-us'} className={'p-4 text-slate-300 font-bold'}>
                 CONTACT US
             </NavLink>
         </nav>
