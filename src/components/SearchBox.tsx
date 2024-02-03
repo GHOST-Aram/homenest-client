b2ee000dfc59addf7c244797afbcdd7cdde66294
GHOST-Aram <owinoojala@gmail.com>
2024-02-03 18:28:22 +0300

    refactor: Change background colors of Input components
             - Change background color of SearchBox, SearchButton and SelectBox Components

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const SearchBox = () => {
     return (
         <input type="text" 
-            className="text-md font-md border-2 border-slate-300 rounded-md p-4 w-full" 
+            className="text-md font-md border-2 border-slate-300 rounded-md p-4 w-full
+            bg-slate-100" 
             placeholder='Search ...'
             aria-label='Search input'
         />
--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -4,11 +4,11 @@ import { FaSearch } from 'react-icons/fa'
 
 const SearchButton = () => {
   return (
-    <button className='bg-primary px-8 py-4 border-2 border-slate-300 rounded-md
-		hover:bg-blue-400 hover:transition-all'
+    <button className='bg-slate-100 px-8 py-4 border-2 border-slate-300 rounded-md
+		text-blue-700 hover:text-slate-300 hover:bg-blue-700 hover:transition-all'
         aria-label='Search Button'
     >
-        <FaSearch className='text-3xl text-slate-300'/>
+        <FaSearch className='text-2xl '/>
     </button>
   )
 }
