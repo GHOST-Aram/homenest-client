87340b65cfb3ab528672f9f034df7f5559b73fa8
GHOST-Aram <owinoojala@gmail.com>
2024-02-05 21:45:45 +0300

    fix: Fix issue in rendering SelectBox
        - Set default value with the defaultValue attribute
        - Remove the selected attribute from SelectBox component

--- src/components/SelectBox.tsx
+++ src/components/SelectBox.tsx
@@ -6,7 +6,7 @@ const SelectBox = () => {
             aria-label='filter search' 
             className=' p-4 border-2 rounded-md border-slate-300 text-slate-900 '
         >
-            <option value="all_listings" selected>All Listings</option>
+            <option defaultValue="all_listings">All Listings</option>
             <option value="city_name">City Name</option>
             <option value="number_bedrooms">Number of Bedrooms</option>
             <option value="estate">Estate</option>
