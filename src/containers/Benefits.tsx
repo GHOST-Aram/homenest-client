8d9baee14664f0b2d76593071a293c350d37a2bd
GHOST-Aram <owinoojala@gmail.com>
2024-02-07 18:35:42 +0300

    refactor: Change display styles of the benefits cards section
            - Display the cards in a responsive grid

--- src/containers/Benefits.tsx
+++ src/containers/Benefits.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactComponentElement, useState} from 'react'
 import BenefitCard from '../components/BenefitCard'
 import { FaSearch, FaStarHalfAlt } from 'react-icons/fa'
 import { PiMapPinFill } from "react-icons/pi";
@@ -6,46 +6,75 @@ import { IoChatbubblesOutline } from "react-icons/io5";
 import { VscChecklist } from "react-icons/vsc";
 import SectionHeading from '../components/SectionHeading'
 import Section from '../components/Section';
+import uniqid from 'uniqid'
 
 
 const Benefits = () => {
+    const [benefits, setBenefits] = useState<Benefit[]>(benefitsArr)
     return (
         <Section>
             <SectionHeading>The Fun Part of Using Homenest.</SectionHeading>
-            <div className="flex flex-row gap-4 overflow-x-scroll pb-4 horizontal-scroll">
-                <BenefitCard 
-                    title='Powerful Search'  
-                    textContent='Filter thousands of listings by location, price, amenities, and more.'
-                    icon={<FaSearch className='font-extrabold text-5xl m-auto text-slate-900'/>}
-                    buttonLabel='SEARCH LISTINGS'
-                />
-                <BenefitCard 
-                    title='Stress Free Browsing'  
-                    textContent=' Explore neighborhoods virtually with our interactive map.'
-                    icon={<PiMapPinFill className='font-extrabold text-5xl m-auto text-slate-900'/>}
-                    buttonLabel='OPEN MAP'
-                />
-                <BenefitCard 
-                    title='Informed Decisions'  
-                    textContent='Read tenant reviews and ratings before you apply.'
-                    icon={<FaStarHalfAlt className='font-extrabold text-5xl m-auto text-slate-900'/>}
-                    buttonLabel='READ REVIEWS'
-                />
-                <BenefitCard 
-                    title='Seamless Communication'  
-                    textContent=' Connect directly with landlords through our secure platform.'
-                    icon={<IoChatbubblesOutline className='font-extrabold text-5xl m-auto text-slate-900'/>}
-                    buttonLabel='SEE LANDLORDS'
-                />
-                <BenefitCard 
-                    title='Effortless Apllications'  
-                    textContent='Submit your application and upload documents online.'
-                    icon={<VscChecklist className='font-extrabold text-5xl m-auto text-slate-900'/>}
-                    buttonLabel='SEE AVAILABE PROPERTIES'
-                />
+            <div className="grid-auto pb-4">
+                {
+                    benefits.length > 0 && benefits.map(benefit =>(
+                        <BenefitCard 
+                            title={benefit.title} 
+                            buttonLabel={benefit.buttonLabel}
+                            textContent={benefit.textContent}
+                            icon={benefit.icon}
+                        />
+
+                    ))
+                }
+               
             </div>
         </Section>
     )
+}
+
+const benefitsArr: Benefit[] = [
+    {
+        id: uniqid(),
+        title: 'Powerful Search',  
+        textContent: 'Filter thousands of listings by location, price, amenities, and more.',
+        icon: <FaSearch className='font-extrabold text-5xl m-auto text-slate-900'/>,
+        buttonLabel: 'SEARCH LISTINGS'
+    },
+    {
+        id: uniqid(),
+        title: 'Stress Free Browsing'  ,
+        textContent: ' Explore neighborhoods virtually with our interactive map.',
+        icon: <PiMapPinFill className='font-extrabold text-5xl m-auto text-slate-900'/>,
+        buttonLabel: 'OPEN MAP',
+    },
+    {
+        id: uniqid(),
+        title: 'Informed Decisions'  ,
+        textContent: 'Read tenant reviews and ratings before you apply.',
+        icon: <FaStarHalfAlt className='font-extrabold text-5xl m-auto text-slate-900'/>,
+        buttonLabel: 'READ REVIEWS'
+    },{
+        id: uniqid(),
+        title: 'Seamless Communication'  ,
+        textContent: ' Connect directly with landlords through our secure platform.',
+        icon: <IoChatbubblesOutline className='font-extrabold text-5xl m-auto text-slate-900'/>,
+        buttonLabel: 'SEE LANDLORDS'
+    },
+    {
+        id: uniqid(),
+        title: 'Effortless Apllications'  ,
+        textContent: 'Submit your application and upload documents online.',
+        icon: <VscChecklist className='font-extrabold text-5xl m-auto text-slate-900'/>,
+        buttonLabel:'SEE AVAILABE PROPERTIES'
+    }
+]
+
+interface Benefit{
+    id: string,
+    title: string,
+    textContent: string,
+    icon: ReactComponentElement<any>,
+    buttonLabel: string
 }
 
 export default Benefits
