55e6e09d390a5dc1e5398f717717a5f089bfbd66
GHOST-Aram <owinoojala@gmail.com>
2024-03-09 22:00:20 +0300

    refactor: Rename authentication cookie

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -3,7 +3,7 @@ import Cookies from "universal-cookie"
 export const setAuthenticationCookie = (tokenExpiry: number, authToken: string) =>{
     const cookie = new Cookies(null, { path: '/'})
     
-    cookie.set('authenticationToken', authToken, {
+    cookie.set('homenestAuthenticationToken', authToken, {
         expires: new Date(tokenExpiry * 1000)
     })
 }
