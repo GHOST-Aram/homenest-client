a08195ab92e88b6e664179eeab1e6778138a16d2
GHOST-Aram <owinoojala@gmail.com>
2024-03-10 12:21:23 +0300

    refactor: Display details of current user in profile page
          - Remove the place holder texts and get user details from autContext
          - Remove initials from the Avatat

---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Avatar from '@mui/material/Avatar'
+
+const UserAvatar = () => {
+    return (
+        <Avatar sx={{
+            height: 200, 
+            width: 200,
+            backgroundColor: '#171717'
+        }}></Avatar>
+    )
+}
+
+export default UserAvatar
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,36 +1,38 @@
 import Header from "../containers/Header"
-import { Avatar } from "@mui/material"
 import LogoutButton from "../components/LogoutButton"
 import NavButton from "../components/NavButton"
+import UserAvatar from "../components/UserAvatar"
+import { useContext } from "react"
+import { AuthContext } from "../utils/authContext"
 
 const Profile = () => {
-  return (
-    <div>
-        <Header />
-        <div className="p-8">
-            <div className="flex flex-col lg:flex-row lg:w-3/5 m-auto items-center 
-                justify-between"
-            >
-                <div className="mt-8 space-y-4">
-                    <Avatar sx={{
-                        height: 200, 
-                        width: 200,
-                        backgroundColor: '#171717'
-                    }}>JD</Avatar>
+    const authContext = useContext(AuthContext)
+    const user = authContext.user ? authContext.user : ''
 
-                    <div className="mt-4">
-                        <div className="text-slate-800 font-bold text-lg">John Doe</div>
-                        <div className="text-slate-600 font-light text-sm">Landlord</div>
+    return (
+        <div>
+            <Header />
+            <div className="p-8">
+                <div className="flex flex-col lg:flex-row lg:w-3/5 m-auto items-center 
+                    justify-between"
+                >
+                    <div className="mt-8 space-y-4">
+                        <UserAvatar/>
+                        <div className="mt-4">
+                            <div className="text-slate-800 font-bold text-lg">{user.name}</div>
+                            <div className="text-slate-600 font-light text-sm">
+                                {user.role === 'landlord' ? 'Landlord' : 'Tenant'}
+                            </div>
+                        </div>
+                        <LogoutButton />
                     </div>
-                    <LogoutButton />
-                </div>
-                <div className="space-y-4 flex flex-col">
-                    <NavButton location="/listings/new" label="Add New Property" color='success' />
-                    <NavButton location={'/profile/listings'} label="My Listings" />
+                    <div className="space-y-4 flex flex-col">
+                        <NavButton location="/listings/new" label="Add New Property" color='success' />
+                        <NavButton location={'/profile/listings'} label="My Listings" />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
   )
 }
 
