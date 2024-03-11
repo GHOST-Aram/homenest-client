e68f48d6db3d973cce8811e60e025e749331e9d1
GHOST-Aram <owinoojala@gmail.com>
2024-03-11 13:37:24 +0300

    chore: Modify styles on PropertyCard component

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import ButtonFilled from './ButtonFilled'
+import { MdLocationPin } from "react-icons/md";
+import Button from '@mui/material/Button'
 import Image from './Image'
 import { useNavigate } from 'react-router-dom'
+import { IoBedSharp } from "react-icons/io5";
+import { FaMoneyBillWave } from "react-icons/fa";
 
 const FeaturedCard = ({ id, rentPm, location, bedrooms, imageSrc }: FeaturedCardProps) => {
     const navigate = useNavigate()
@@ -12,26 +14,29 @@ const FeaturedCard = ({ id, rentPm, location, bedrooms, imageSrc }: FeaturedCard
     }
     
     return (
-        <div className="min-w-80 rounded-md border-2 flex flex-col justify-between">
+        <div className="min-w-80 rounded-md flex flex-col justify-between">
             <Image 
                 src={imageSrc} 
                 alt={`${location} ${bedrooms} bedroom house`}
                 className='rounded-t-md'
             />           
-            <div className="bg-slate-400 px-4 w-full rounded-b-md">
-                <p className='space-x-2'> 
-                    <span className='font-bold text-slate-700'>Rent per Month:</span> 
-                    <span className='font-bold'>{rentPm}</span> </p>
-                <p className='space-x-2'> 
-                    <span className='font-bold text-slate-700'>Location:</span> 
-                    <span className='font-bold'>{location}</span> </p>
-                <p className='space-x-2'> 
-                    <span className='font-bold text-slate-700'>Bedrooms:</span> 
-                    <span className='font-bold'>{bedrooms}</span> </p>
+            <div className=" px-4 w-full rounded-b-md">
+                <p className='space-x-2 flex items-center'> 
+                    <FaMoneyBillWave  className="text-slate-600 font-light text-sm"/>
+                    <span className='font-light'>Ksh. {rentPm} per month</span> </p>
+                <p className='space-x-2 flex items-center'> 
+                    <MdLocationPin className="text-slate-600 font-light text-sm"/>
+                    <span className='font-light'>{location}</span> </p>
+                <p className='space-x-2 flex items-center'> 
+                    <IoBedSharp className="text-slate-600 font-light text-sm"/>
+                    <span className='font-light'>{bedrooms} Bedrooms</span> </p>
                 <div className="text-center py-4">
-                    <ButtonFilled className='w-full'
+                    <Button 
+                        fullWidth
+                        size='large'
+                        variant='contained'
                         onClick={navigateToDetails}
-                    >VIEW DETAILS</ButtonFilled>
+                    >VIEW DETAILS</Button>
                 </div>
             </div>
         </div>
