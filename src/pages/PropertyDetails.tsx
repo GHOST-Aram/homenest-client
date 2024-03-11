c9cc585ba9be8f486b654e8f9c8e92625ec25063
GHOST-Aram <owinoojala@gmail.com>
2024-03-11 08:48:27 +0300

    feat: Fetch property by id in details page
         - Extract id from url params
         - Append the Id to the properties endpoint URL and fetch data

--- src/pages/PropertyDetails.tsx
+++ src/pages/PropertyDetails.tsx
@@ -8,8 +8,31 @@ import FloorPlan from '../containers/FloorPlan'
 import Map from '../containers/Map'
 import TenantReviews from '../containers/TenantReviews'
 import PropertyCallToAction from '../containers/PropertyCallToAction'
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { getData } from '../utils/fetch'
 
 const PropertyDetails = () => {
+    const [property, setProperty] = useState(null)
+    const { id } = useParams()
+    
+    useEffect(() =>{
+        (async() =>{
+            try {
+                const response = await getData(`http://localhost:8000/properties/${id}`)
+                const statusCode = response.status
+                console.log('Status code: ', statusCode)
+                if(statusCode === 200){
+                    const data = await response.json()
+                    console.log(data)
+                }
+            } catch (error) {
+                console.log(error)
+            }
+                
+
+        })() 
+    })
     return (
         <>
             <DetailsHero />
