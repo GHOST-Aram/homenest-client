23ffe952ea38c081edf9db811cbfab5927cbbca9
GHOST-Aram <owinoojala@gmail.com>
2024-03-11 10:57:20 +0300

    chore: Remove The Benefits section from Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import HeroSection from '../containers/HeroSection';
-import Benefits from '../containers/Benefits';
 import FeaturedListings from '../containers/FeaturedListings';
 import TenantReviews from '../containers/TenantReviews';
 
@@ -7,7 +6,6 @@ const Home = () => {
     return (
         <main>
             <HeroSection />
-            <Benefits />
             <FeaturedListings />
             <TenantReviews />
         </main>
