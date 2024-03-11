ce7aa5331a8ad1dc24b27238cafb43cb3c6ba8f3
GHOST-Aram <owinoojala@gmail.com>
2024-03-11 14:45:44 +0300

    chore: Remove Neigboring amenities section from property details page

--- src/pages/PropertyDetails.tsx
+++ src/pages/PropertyDetails.tsx
@@ -1,7 +1,6 @@
 import DetailsHero from '../containers/DetailsHero'
 import PropertyDescription from '../containers/PropertyDescription'
 import ContactLandlord from '../containers/ContactLandlord'
-import NeighboringAmenities from '../containers/NeighboringAmenities'
 import PropertyGallery from '../containers/PropertyGallery'
 import KeyFeatures from '../containers/KeyFeatures'
 import { useParams } from 'react-router-dom'
@@ -46,8 +45,6 @@ const PropertyDetails = () => {
                 <PropertyDescription description = { property ? property.description : '' }/>
                 <ContactLandlord />
             </section>
-            <Divider/>
-            <NeighboringAmenities />
             <Divider/>
             <PropertyGallery />
         </>
