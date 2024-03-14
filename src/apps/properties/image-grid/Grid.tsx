874847fe0e1dd3f3de8f5865b7006ccfa924befc
GHOST-Aram <owinoojala@gmail.com>
2024-03-14 14:03:49 +0300

    refactor: Remove window.location condition checking from Gallery grid
             - Display delete icons on iamged without conditions

--- src/apps/properties/image-grid/Grid.tsx
+++ src/apps/properties/image-grid/Grid.tsx
@@ -1,46 +1,46 @@
+import Box from '@mui/material/Box'
 import IconButton from '@mui/material/IconButton'
-import Image from '../../../components/Image'
 import { MdDelete } from 'react-icons/md'
 import { GalleryItem } from '../../../types'
 
 const Grid = ({
     images,
-    isDisplayingOnForm,
     deleteImage
 }:Props) => {
-  return (
-    <div>
-        <div className="grid-auto">
+	return (
+		<Box className="grid-auto">
 			{
 				images.length > 0 && 
 					images.map((image, index) => (
-						<div key={image?._id ? image._id : `${image.url}-$${index}`}>
-							{
-								isDisplayingOnForm && deleteImage &&
-								<IconButton 
-									onClick={()=> deleteImage(image)}
-									color='error'
-									size='large' 
-									className='absolute top-12'
-								>
-									<MdDelete />
-								</IconButton>
+						<Box key={
+								image?._id ? image._id 
+								: `${image.url}-$${index}`
 							}
-							<Image src={image.url} alt={image.alt} 
+						>
+							
+							<IconButton 
+								onClick={()=> deleteImage(image)}
+								color='error'
+								size='large' 
+								className='absolute top-12'
+							>
+								<MdDelete />
+							</IconButton>
+							<img 
+								src={image.url} 
+								alt={image.alt} 
 								className='w-full' 
 							/>
-						</div>
+						</Box>
 					))
 			}
-		</div>
-    </div>
-  )
+		</Box>
+	)
 }
 
 interface Props {
     images: GalleryItem[]
-    deleteImage?:(image: GalleryItem)=>void
-    isDisplayingOnForm: boolean
+    deleteImage:(image: GalleryItem)=>void
 }
 
 
--- src/apps/properties/image-grid/ImageGrid.tsx
+++ src/apps/properties/image-grid/ImageGrid.tsx
@@ -1,24 +1,16 @@
-import { useLocation } from 'react-router-dom'
 import { GalleryItem } from '../../../types'
 import Grid from './Grid'
 
 const ImageGrid = ({images, deleteImage}:Props) => {
-	const location = useLocation()
-	const pathname = location.pathname
-	const isDisplayingOnForm = pathname.includes('new') || pathname.includes('update')
 
 	return (
-		<Grid 
-			images={images}
-			isDisplayingOnForm={isDisplayingOnForm}
-			deleteImage={deleteImage}
-		/>
+		<Grid images={images} deleteImage={deleteImage} />
 	)
 }
 
 interface Props {
 	images: GalleryItem[]
-	deleteImage?:(image: GalleryItem)=>void
+	deleteImage:(image: GalleryItem)=>void
 }
 
 export default ImageGrid
