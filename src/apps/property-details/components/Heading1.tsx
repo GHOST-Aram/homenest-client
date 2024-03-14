a1bdafa87517e67a093a911186cbc057b178fc81
GHOST-Aram <owinoojala@gmail.com>
2024-03-14 16:18:28 +0300

    refactor: Encapsulate fields array in SignUp form
             - Create a helprt function that accepts userData and returns an array
               of field Objects with userData

--- src/apps/property-details/components/Heading1.tsx
+++ src/apps/property-details/components/Heading1.tsx
@@ -3,10 +3,11 @@ import {ReactNode} from 'react'
 
 const Heading1 = ({ children }: { children: ReactNode}) => {
   return (
-    <h1 className='font-bold text-slate-300 text-4xl text-center'>
+    <h1 className={heading}>
         { children }
     </h1>
   )
 }
+const heading = 'font-bold text-slate-300 text-4xl text-center'
 
 export default Heading1
--- src/apps/sign-up/containers/SignUpForm.tsx
+++ src/apps/sign-up/containers/SignUpForm.tsx
@@ -2,25 +2,27 @@ import TextField from '@mui/material/TextField'
 import RolesRadioInput from '../components/RolesRadioInput'
 import SignUpButton from '../components/SignUpButton'
 import StatusAlert from '../components/SignUpStatusAlert'
-import { FormProps } from '../../../types'
+import { FormProps, UserData } from '../../../types'
 import GoToLoginBtn from '../components/GoToLoginBtn'
 import Form from '../../../components/Form'
 
-const SignUpForm = ({ userData, registerUser, status, changeHandler }: FormProps) => {
-    const fields = [
-        { name:'fullName',label: 'Full name', value: userData.fullName, type: 'text' },
-        { name:'email',label: 'Email', value: userData.email , type: 'email'},
-        {  name:'password',label: 'Password', value: userData.password , type: 'password'},
-        { name:'confirmPassword',label: 'Confirm Password', value: userData.confirmPassword, 
-            type: 'password'}, 
-    ]
-
+const SignUpForm = (
+    { 
+        userData, 
+        registerUser, 
+        status, 
+        changeHandler 
+    }: FormProps
+) => {
+    
+    const fields = createFieldsWithUserData(userData)
 
     const signUp = async() => {
         if(userData.password === userData.confirmPassword){
             await registerUser()
         }
     }
+
     return (
         <Form heading='Welcome to Homenest' submitHandler={ signUp }>
             {
@@ -39,18 +41,43 @@ const SignUpForm = ({ userData, registerUser, status, changeHandler }: FormProps
             }
             {
                 userData.password !== userData.confirmPassword &&
-                <p className="text-sm text-red-700">Passwords should be identical</p>
+                <p className={errorMsg}>Passwords should be identical</p>
             }
             <RolesRadioInput value={userData.role} changeHandler={changeHandler} />
             <StatusAlert status={status}/>
             <SignUpButton status={status}/>
-            <p className="font-bold text-center text-slate-800">OR</p>
+            <p className={ boldText }>OR</p>
             <GoToLoginBtn/>
         </Form>
     )
 }
 
 
+const createFieldsWithUserData = (userData: UserData) =>{
+    return [
+        { 
+            name:'fullName', label: 'Full name', 
+            value: userData.fullName, 
+            type: 'text' 
+        },
+        { 
+            name:'email', label: 'Email', 
+            value: userData.email , 
+            type: 'email'
+        },
+        {  
+            name:'password', label: 'Password', 
+            value: userData.password , 
+            type: 'password'
+        },
+        { 
+            name:'confirmPassword', label: 'Confirm Password', 
+            value: userData.confirmPassword, type: 'password'
+        }, 
+    ]
+}
 
+const errorMsg = "text-sm text-red-700"
+const boldText  = "font-bold text-center text-slate-800"
 
 export default SignUpForm
