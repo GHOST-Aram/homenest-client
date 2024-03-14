f0d35bc611d81edb404d7d1f838c7a728483d5ad
GHOST-Aram <owinoojala@gmail.com>
2024-03-14 11:41:27 +0300

    refactor: Replace divs with @mui Box component on property           details hero section

--- src/apps/property-details/containers/DetailsHero.tsx
+++ src/apps/property-details/containers/DetailsHero.tsx
@@ -2,6 +2,7 @@ import { GiMoneyStack } from "react-icons/gi";
 import { IoBedSharp } from "react-icons/io5";
 import { FaMap } from "react-icons/fa";
 import { IoLocationSharp } from "react-icons/io5";
+import Box from '@mui/material/Box'
 
 const DetailsHero = ({
     propertyName,
@@ -15,35 +16,35 @@ const DetailsHero = ({
 }: Props) => {
   return (
     <section className='hero' style={{backgroundImage : `url(${backgroundImageUrl})`}}>
-        <div className="w-full bg-slate-400 h-[70vh] py-8 space-y-4 overlay">
+        <Box className="w-full bg-slate-400 h-[70vh] py-8 space-y-4 overlay">
             <h1 className='font-bold text-slate-300 text-4xl text-center'>
                 { propertyName }
             </h1>
-            <div className="flex flex-row gap-4 justify-center">
+            <Box className="flex flex-row gap-4 justify-center">
                 <GiMoneyStack className='text-slate-300 text-4xl' />
                 <h2 className='font-md text-slate-300 text-3xl text-center'>
                     Ksh { rentPerMonth } per Month | Ksh { rentPerYear } per year
                 </h2>
-            </div>
-            <div className="flex flex-row gap-4 justify-center">
+            </Box>
+            <Box className="flex flex-row gap-4 justify-center">
                 <IoBedSharp className='text-slate-300 text-4xl' />
                 <h2 className='font-md text-slate-300 text-3xl text-center'>
                     {bedrooms} bedrooms | { bathrooms } bathrooms
                 </h2>
-            </div>
-            <div className="flex flex-row gap-4 justify-center">
+            </Box>
+            <Box className="flex flex-row gap-4 justify-center">
                 <FaMap className='text-slate-300 text-4xl' />
                 <h2 className='font-md text-slate-300 text-3xl text-center'>
                     {   squareFootage  } sq ft
                 </h2>
-            </div>
-            <div className="flex flex-row gap-4 justify-center">
+            </Box>
+            <Box className="flex flex-row gap-4 justify-center">
                 <IoLocationSharp className='text-slate-300 text-4xl' />
                 <h2 className='font-md text-slate-300 text-3xl text-center'>
                     { locationAddress }
                 </h2>
-            </div>
-        </div>        
+            </Box>
+        </Box>        
     </section>
   )
 }
