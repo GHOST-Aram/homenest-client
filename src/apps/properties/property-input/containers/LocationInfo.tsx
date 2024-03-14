62cb56b67c444890cbe9cd431c80c68e02e137ce
GHOST-Aram <owinoojala@gmail.com>
2024-03-14 14:19:25 +0300

    refactor: Replace the custom TextField component with @mui TextField           in the LocationInfo container

--- src/apps/properties/property-input/containers/LocationInfo.tsx
+++ src/apps/properties/property-input/containers/LocationInfo.tsx
@@ -1,41 +1,51 @@
 import { ChangeEventHandler } from 'react'
 import Box from '@mui/material/Box'
-import MUITextField from '../../../../components/MUITextField'
+import TextField from '@mui/material/TextField'
 
 const LocationInfo = ({
     getTextFieldValue,
     cityOrTown,
     locationName,
     estate
-}: Props) => {
-  return (
-    <>
-        <Box className="flex flex-col gap-4 lg:flex-row justify-between">
-            <MUITextField 
-                type="text" 
-                name="cityOrTown" 
-                value = {cityOrTown} 
-                changeHandler={getTextFieldValue} 
-                label={'City/Town'} 
-            />
-            <MUITextField 
-                type="text" 
-                name="estate" 
-                value = {estate} 
-                changeHandler={getTextFieldValue} 
-                label={'Estate'} 
-            />
-             <MUITextField 
-                name="locationName" 
-                type="text" 
-                label="Location Address"
-                value={locationName}
-                changeHandler={getTextFieldValue}
-            /> 
-        </Box>
-    </>
-  )
+}: Props
+) => {
+
+    return (
+        <>
+            <Box className={flexContainer}>
+                <TextField 
+                    type="text" 
+                    name="cityOrTown" 
+                    value = {cityOrTown} 
+                    onChange={getTextFieldValue} 
+                    label={'City/Town'} 
+                    required
+                    fullWidth
+                />
+                <TextField 
+                    type="text" 
+                    name="estate" 
+                    value = {estate} 
+                    onChange={getTextFieldValue} 
+                    label={'Estate'} 
+                    required
+                    fullWidth
+                />
+                <TextField 
+                    name="locationName" 
+                    type="text" 
+                    label="Location Address"
+                    required
+                    fullWidth
+                    value={locationName}
+                    onChange={getTextFieldValue}
+                /> 
+            </Box>
+        </>
+    )
 }
+
+const flexContainer = "flex flex-col gap-4 lg:flex-row justify-between"
 
 interface Props{
     getTextFieldValue: ChangeEventHandler<HTMLInputElement>
