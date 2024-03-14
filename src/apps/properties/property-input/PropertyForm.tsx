2f4e34d74d52b972986322eb510ea9d34a4d5064
GHOST-Aram <owinoojala@gmail.com>
2024-03-14 15:38:03 +0300

    refactor: Encapsulate the Property form Submit button in SubmitButton component

--- src/apps/properties/property-input/PropertyForm.tsx
+++ src/apps/properties/property-input/PropertyForm.tsx
@@ -1,9 +1,9 @@
 import { ChangeEventHandler, ReactNode, Dispatch,SetStateAction } from 'react'
 import { SelectChangeEvent } from "@mui/material/Select"
 import Box from "@mui/material/Box"
-import Button from '@mui/material/Button'
 import { PropertyData, Status } from '../../../types'
 import StatusAlert from '../../login/components/StatusAlert'
+import SubmitButton from './components/SubmitButton'
 import PropertyProfile from './containers/PropertyProfile'
 import PropertyMetrics from './containers/PropertyMetrics'
 import PropertyAvailabilityAndMore from './containers/PropertyAvailabilityAndMore'
@@ -12,36 +12,33 @@ import GalleryInput from './containers/GalleryInputSection'
 import { useLocation } from 'react-router-dom'
 import DescriptionAndBackgroud from './containers/DescriptionAndBackgroud'
 
-const PropertyForm = ({
-    submitFormData, 
-    propertyData, 
-    getCheckboxValue,
-    getTextFieldValue, 
-    getSelectedValue,
-    setProperty,
-    status
-} : Props) => {
+const PropertyForm = (
+    {
+        propertyData, 
+        submitFormData, 
+        getCheckboxValue,
+        getTextFieldValue, 
+        getSelectedValue,
+        setProperty,
+        status
+    } : Props
+) => {
     const location = useLocation()
     const pathname = location.pathname
     const isUpdateForm = pathname.includes('update')
 
     const submitButtonName =   isUpdateForm ? 'Update Property Data' : 'List Property'
 
-    
     return (
         <div>
-            <form 
-                onSubmit={async(e) =>{
-                    e.preventDefault()
-                    await submitFormData()
-                }}
+            <form onSubmit={async(e) =>{ e.preventDefault(); await submitFormData() }}
+                className={form}
                 aria-labelledby="property-form-title" 
-                className="m-auto py-4 space-y-4 w-4/5"
             >
-                <h1 id="property-form-title" className="text-center text-lg font-bold text-blue-700">
+                <h1 id="property-form-title" className={heading}>
                     {isUpdateForm ? 'Update Property Data': 'New Property Listing'}
                 </h1>
-                <Box className="w-full p-8 border-2 rounded-md space-y-4">
+                <Box className={container}>
                     <PropertyProfile 
                         locationName={propertyData.locationName}
                         propertyName={propertyData.propertyName}
@@ -80,30 +77,26 @@ const PropertyForm = ({
                     getSelectedValue={getSelectedValue}
                 />
                 <StatusAlert status = {status}/>
-                <Button 
-                    variant="contained" 
-                    fullWidth color="success" 
-                    size="large" 
-                    type="submit"
-                    disabled = {status === 'loading'}
-                >
+                <SubmitButton disabled = {status === 'loading'}>
                     { status === 'loading' ? 'Loading' : submitButtonName}
-                </Button>
+                </SubmitButton>
             </form>
         </div>
     )
 }
 
+const form = "m-auto py-4 space-y-4 w-4/5"
+const heading = "text-center text-lg font-bold text-blue-700"
+const container = "w-full p-8 border-2 rounded-md space-y-4"
+
 interface Props{
-    submitFormData: ()=>Promise<void>
+    status : Status
     propertyData: PropertyData
+    submitFormData: ()=>Promise<void>
     getTextFieldValue: ChangeEventHandler
     getCheckboxValue: ChangeEventHandler
     getSelectedValue: (e: SelectChangeEvent<string | string[]>, child: ReactNode) => void
-    status : Status
     setProperty: Dispatch<SetStateAction<PropertyData>>
 }
-
-
 
 export default PropertyForm
