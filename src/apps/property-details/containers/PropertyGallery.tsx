1df9fec12412af992067218116c8891ee2faff2c
GHOST-Aram <owinoojala@gmail.com>
2024-03-14 13:29:10 +0300

    refactor: Create a separate Image grid for property details page
             - Create a grid that is not shared with the property forms

--- src/apps/property-details/containers/PropertyGallery.tsx
+++ src/apps/property-details/containers/PropertyGallery.tsx
@@ -1,18 +1,18 @@
-import Heading from '../../../components/Heading'
-import ImageGrid from '../../properties/image-grid/ImageGrid'
+import ImageGrid from '../components/ImageGrid'
 import { GalleryItem } from '../../../types'
 
 
 const PropertyGallery = ({ images }: {images: GalleryItem[]}) => {
 
     return (
-        <section className='px-8 py-4 space-y-4'>
-            <Heading level={1} className="text-xl font-bold text-slate-800">
-                Property Gallery
-            </Heading>
+        <section className={ section }>
+            <h1 className={heading}>Property Gallery</h1>
             <ImageGrid images={images}/>
         </section>
     )
 }
+
+const heading = 'text-xl font-bold text-slate-800'
+const section = 'px-8 py-4 space-y-4'
 
 export default PropertyGallery
