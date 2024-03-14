c52d95d4337e96e366a0261467cc46d416caff09
GHOST-Aram <owinoojala@gmail.com>
2024-03-14 11:57:52 +0300

    refactor: Create variables for class names in Property DetailsHero           Container component

--- src/apps/property-details/containers/DetailsHero.tsx
+++ src/apps/property-details/containers/DetailsHero.tsx
@@ -16,38 +16,47 @@ const DetailsHero = ({
     locationAddress,
     backgroundImageUrl
 }: Props) => {
-  return (
-    <section className='hero' style={{backgroundImage : `url(${backgroundImageUrl})`}}>
-        <Box className="w-full bg-slate-400 h-[70vh] py-8 space-y-4 overlay">
-            <Heading1> { propertyName } </Heading1>
-            <Box className="flex flex-row gap-4 justify-center">
-                <GiMoneyStack className='text-slate-300 text-4xl' />
-                <Heading2>
-                    Ksh { rentPerMonth } per Month | Ksh { rentPerYear } per year
-                </Heading2>
-            </Box>
-            <Box className="flex flex-row gap-4 justify-center">
-                <IoBedSharp className='text-slate-300 text-4xl' />
-                <Heading2>
-                    {bedrooms} bedrooms | { bathrooms } bathrooms
-                </Heading2>
-            </Box>
-            <Box className="flex flex-row gap-4 justify-center">
-                <FaMap className='text-slate-300 text-4xl' />
-                <Heading2>
-                    {   squareFootage  } sq ft
-                </Heading2>
-            </Box>
-            <Box className="flex flex-row gap-4 justify-center">
-                <IoLocationSharp className='text-slate-300 text-4xl' />
-                <Heading2>
-                    { locationAddress }
-                </Heading2>
-            </Box>
-        </Box>        
-    </section>
-  )
+    
+    return (
+        <section 
+            className='hero' 
+            style={{backgroundImage : `url(${backgroundImageUrl})`}}
+        >
+            <Box className={overlayContainer}>
+                <Heading1> { propertyName } </Heading1>
+
+                <Box className={flexContainer}>
+                    <GiMoneyStack className={iconClassName} />
+                    <Heading2>
+                        Ksh { rentPerMonth } per Month | Ksh { rentPerYear } per year
+                    </Heading2>
+                </Box>
+                <Box className={flexContainer}>
+                    <IoBedSharp className={iconClassName} />
+                    <Heading2>
+                        {bedrooms} bedrooms | { bathrooms } bathrooms
+                    </Heading2>
+                </Box>
+                <Box className={flexContainer}>
+                    <FaMap className={iconClassName} />
+                    <Heading2>
+                        {   squareFootage  } sq ft
+                    </Heading2>
+                </Box>
+                <Box className={flexContainer}>
+                    <IoLocationSharp className={iconClassName} />
+                    <Heading2>
+                        { locationAddress }
+                    </Heading2>
+                </Box>
+            </Box>        
+        </section>
+    )
 }
+
+const iconClassName = 'text-slate-300 text-4xl'
+const flexContainer = 'flex flex-row gap-4 justify-center'
+const overlayContainer = "w-full bg-slate-400 h-[70vh] py-8 space-y-4 overlay"
 
 interface Props{
     propertyName: string
