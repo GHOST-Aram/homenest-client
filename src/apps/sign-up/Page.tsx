f506bec68f9b2fd81ff2c13d2fe3f05688a3fb44
GHOST-Aram <owinoojala@gmail.com>
2024-03-15 12:02:17 +0300

    refactor: Enapsulate signup logic in a class
             - Create a class called UserSignUp that accepts states, state setters and
               Navigate function as parameters to its constructor
             - Use the class to manage signup

--- src/apps/sign-up/Page.tsx
+++ src/apps/sign-up/Page.tsx
@@ -1,13 +1,11 @@
 import Paper from '@mui/material/Paper'
-import { ChangeEvent, useState } from 'react'
+import { useState } from 'react'
 import SignUpForm from './containers/SignUpForm'
 import { UserData, Status } from '../../types'
 import { useNavigate } from 'react-router'
 import Box from '@mui/material/Box'
-import { sendPostRequest } from '../../utils/fetch'
-import { updateProcessStatus } from '../../utils/process-status'
-import { validateUserData } from '../../utils/validator'
-import { ValidationError } from 'yup'
+import { UserSignUp } from './sign-up'
+
 
 export const SignUp = () =>{
     const navigate = useNavigate()
@@ -22,66 +20,21 @@ export const SignUp = () =>{
         confirmPassword:''
     })
 
-    const collectUserInput = (e: ChangeEvent<HTMLInputElement>) =>{
-        const { name, value } = e.target
-        setUserData({...userData, [name]: value })
-    }
-
-    const submitUserData = () =>{
-        
-        (async() =>{
-            setStatus('loading')
-
-            try {
-                await validateUserData(userData)
-
-                const{ statusCode, body } = await createUser()
-
-                processResponse( { statusCode, body })
-                
-            } catch (error) {
-                if(error instanceof ValidationError){
-                    setStatus('invalid-input')
-                    setErrorMsg(error.message)
-                } else{
-                    setStatus('error')            
-                }
-            }
-        })()
-    }
-
-    const createUser = async() =>{
-        const response = await sendPostRequest('http://localhost:8000/users', 
-        userData)
-
-        const statusCode = response.status
-        const body = await response.json()
-
-        return {statusCode, body }
-    }
-
-    const processResponse = ({ statusCode, body }: {statusCode: number, body: any }) =>{
-        updateProcessStatus(setStatus, statusCode)
-
-        if(statusCode === 400){
-            setErrorMsg(body.errors[0].msg)
-            console.log(errorMsg)
-        } else if(statusCode === 201){
-            gotToLoginPage()
-        }
-    }
-
-    const gotToLoginPage = () =>{
-        navigate('/login')
-    }
+    const signUp = new UserSignUp({
+        userData,
+        setUserData,
+        setStatus,
+        setErrorMsg,
+        navigate,
+    })
     
     return(
         <Box className="my-8">
             <Paper elevation={8} className={container}>
                 <SignUpForm 
                     status={status} 
-                    changeHandler={collectUserInput} 
-                    onSubmit={submitUserData}
+                    changeHandler={signUp.collectUserInput} 
+                    onSubmit={signUp.submitUserData}
                     userData={userData}
                     errorMsg={errorMsg}
                 />            
