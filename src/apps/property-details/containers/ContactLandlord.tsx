dd2f4383cdd5c327571b8f647b21dd2d019b35be
GHOST-Aram <owinoojala@gmail.com>
2024-03-26 07:50:21 +0300

    refactor: Add contact details on property details page

--- src/apps/property-details/containers/ContactLandlord.tsx
+++ src/apps/property-details/containers/ContactLandlord.tsx
@@ -1,4 +1,4 @@
-import ActionButton from '../components/ActionButton'
+import { MdMail, MdCall, MdWhatsapp } from 'react-icons/md'
 import Box from '@mui/material/Box'
 
 
@@ -9,9 +9,27 @@ const ContactLandlord = () => {
                 Contact the Landlord
             </h1>
             <Box className={btnsContainer}>
-                <ActionButton onClick={()=>{}}>Send Direct Message</ActionButton>
-                <ActionButton onClick={()=>{}}>Schedule Viewing</ActionButton>
-                <ActionButton onClick={()=>{}}>Apply Online</ActionButton>
+                <a 
+                    className="flex flex-row items-center gap-4 px-4 py-2 rounded-md bg-blue-300"
+                    href='mailto:email@gmail.com'
+                >
+                    <MdMail className='text-orange-600 text-lg'/>
+                    <p className="text-slate-800 font-light">Email monalisa@gmail.com</p>
+                </a>
+                <a 
+                    className="flex flex-row items-center gap-4 px-4 py-2 rounded-md bg-blue-300"
+                    href='https://wa.me/+254798899809'
+                >
+                    <MdWhatsapp className='text-green-600 text-lg'/>
+                    <p className="text-slate-800 font-light">WhatsApp +25479666998</p>
+                </a>
+                <a 
+                    className="flex flex-row items-center gap-4 px-4 py-2 rounded-md bg-blue-300"
+                    href='tel:+254798899809'
+                >
+                    <MdCall className='text-slate-800 text-lg'/>
+                    <p className="text-slate-800 font-light">Call +254796699956</p>
+                </a>
             </Box>
         </Box>
     )
