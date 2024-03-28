f795016448455996644c77d0451f75ebd73ff280
GHOST-Aram <owinoojala@gmail.com>
2024-03-28 17:30:46 +0300

    refactor: Provide links with landlords contact details

--- src/apps/property-details/containers/ContactLandlord.tsx
+++ src/apps/property-details/containers/ContactLandlord.tsx
@@ -1,27 +1,15 @@
 import { MdMail, MdCall, MdWhatsapp } from 'react-icons/md'
 import Box from '@mui/material/Box'
-import Messenger from '../../messenger/Messenger'
 
 
 const ContactLandlord = () => {
-    const [isMessagingOpen, setIsMessagingOpen] = useState<boolean>(false)
 
-    const openMessaging = () =>{
-        setIsMessagingOpen(true)
-    }
-
-    const closeMessaging = () =>{
-        setIsMessagingOpen(false)
-    }
     return (
         <Box>
             <h1 className={heading}>
                 Contact the Landlord
             </h1>
             <Box className={btnsContainer}>
-                <ActionButton onClick={openMessaging}>Send Direct Message</ActionButton>
-                <ActionButton onClick={()=>{}}>Schedule Viewing</ActionButton>
-                <ActionButton onClick={()=>{}}>Apply Online</ActionButton>
                 <a 
                     className="flex flex-row items-center gap-4 px-4 py-2 rounded-md bg-blue-300"
                     href='mailto:email@gmail.com'
@@ -34,17 +22,16 @@ const ContactLandlord = () => {
                     href='https://wa.me/+254798899809'
                 >
                     <MdWhatsapp className='text-green-600 text-lg'/>
-                    <p className="text-slate-800 font-light">WhatsApp +25479666998</p>
+                    <p className="text-slate-800 font-light">WhatsApp +254796669XXX8</p>
                 </a>
                 <a 
                     className="flex flex-row items-center gap-4 px-4 py-2 rounded-md bg-blue-300"
                     href='tel:+254798899809'
                 >
                     <MdCall className='text-slate-800 text-lg'/>
-                    <p className="text-slate-800 font-light">Call +254796699956</p>
+                    <p className="text-slate-800 font-light">Call +25479XXX956</p>
                 </a>
             </Box>
-            { isMessagingOpen && <Messenger close = { closeMessaging }/> }
         </Box>
     )
 }
