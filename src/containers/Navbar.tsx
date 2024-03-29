a5f4cd85b13f1f706167414940d95f83dce939c4
GHOST-Aram <owinoojala@gmail.com>
2024-03-29 21:41:41 +0300

    refactor: Put Login and Sign Up links in a dropdown menu
             - Use a MUI Dropdown component to create an Accounts dropdown menu for
               Login and Sign up

--- src/containers/Navbar.tsx
+++ src/containers/Navbar.tsx
@@ -1,15 +1,20 @@
 import { useContext } from 'react'
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink, Link, useNavigate, useLocation } from 'react-router-dom'
 import { AuthContext } from '../utils/authContext'
 import Avatar from '@mui/material/Avatar'
+import { Dropdown } from '@mui/base/Dropdown';
+import { MenuButton } from '@mui/base/MenuButton';
+import { Menu } from '@mui/base/Menu';
+import { MenuItem } from '@mui/base/MenuItem';
+import Divider from '@mui/material/Divider'
 
 const Navbar = () => {
     const context = useContext(AuthContext)
 
     return (
         <nav className={container}>
-            <NavLink to={'/'} className={navLink}>HOME</NavLink>
-            <NavLink to={'/listings'} className={navLink}>LISTINGS</NavLink>
+            <NavLink to={'/'} className={'nav-link'}>HOME</NavLink>
+            <NavLink to={'/listings'} className={'nav-link'}>LISTINGS</NavLink>
             {
                 context && context.user ?
                 <Link to='/profile' className={profileLink}>
@@ -18,17 +23,55 @@ const Navbar = () => {
                 </Link>
                  :
                 <>
-                    <NavLink to={'/sign-up'} className={navLink}>SIGN UP</NavLink>
-                    <NavLink to={'/login'} className={navLink}>LOGIN</NavLink>
+                    {/* <NavLink to={'/sign-up'} className={'nav-link'}>SIGN UP</NavLink>
+                    <NavLink to={'/login'} className={'nav-link'}>LOGIN</NavLink> */}
+                    <AccountsMenu />
                 </>
             }
         </nav>
     )
 }
 
+const AccountsMenu = () =>{
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const accountsIsActive = /(sign-up|login)/i.test(location.pathname)
+    const loginIsActive =  /login/i.test(location.pathname)
+    const signUpIsActive =  /sign-up/i.test(location.pathname)
+
+    const goToSignUp = () =>{
+        navigate('/sign-up')
+    }
+
+    const goToLogin = () =>{
+        navigate('/login')
+    }
+
+    return(
+        <Dropdown>
+            <MenuButton className={`nav-link ${accountsIsActive ? 'active': ''}`}>ACCOUNT</MenuButton>
+            <Menu className='accounts-menu'>
+                <MenuItem onClick={goToSignUp} 
+                    className={`accounts-menu-item ${signUpIsActive ? 'active': ''}`}
+                >
+                    SIGN UP
+                </MenuItem>
+                <hr />
+                <MenuItem onClick={goToLogin} 
+                    className={`accounts-menu-item ${loginIsActive ? 'active': ''}`}
+                >
+                    LOG IN
+                </MenuItem>
+            </Menu>
+        </Dropdown>
+    )
+}
+
+
+
 const container = 'flex flex-row gap-4 items-center md:justify-center'+
     'md:flex'
-const navLink = 'p-4 text-slate-300 font-bold'
 const profileLink = "flex items-center gap-4 p-2 bg-blue-800 rounded-md"
 const userName= "text-slate-300 font-bold"
 
