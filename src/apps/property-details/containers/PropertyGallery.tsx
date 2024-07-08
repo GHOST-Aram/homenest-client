98d35b7692dcb524317dc343a53b4e3725177e29
GHOST-Aram <owinoojala@gmail.com>
2024-07-08 12:46:09 +0300

    feat: Implement  Gallery images upload
          - Preview images after selection
          - Send Post request to property gallaries url

--- src/apps/property-details/containers/PropertyGallery.tsx
+++ src/apps/property-details/containers/PropertyGallery.tsx
@@ -2,7 +2,8 @@ import ImageGrid from '../components/ImageGrid'
 import { GalleryItem } from '../../../types'
 import FileSelector from '../../properties/property-input/containers/form-sections/FileSelector'
 import { useState } from 'react'
-
+import { Button } from '@mui/material'
+import { useParams } from 'react-router-dom'
 
 interface Preview{
     id: string
@@ -10,7 +11,7 @@ interface Preview{
 }
 
 const PropertyGallery = ({ images }: {images: GalleryItem[]}) => {
-
+    const { id } = useParams()
     const [previews, setPreviews] = useState<Preview[]>([])
     const [imageFiles, setImageFiles] = useState<File[]>([])
 
@@ -32,6 +33,37 @@ const PropertyGallery = ({ images }: {images: GalleryItem[]}) => {
     const addFile = (file: File) =>{
         setImageFiles([...imageFiles, file ])
     }
+
+    const submitFiles = async() =>{
+        const formData = new FormData()
+
+        formData.append('assetId', id as string)
+
+        imageFiles.forEach(file =>{
+            formData.append('images', file)
+        })
+
+        if(imageFiles.length){
+            console.log(imageFiles)
+
+            try{
+                const response = await fetch(`${API_BASE_URL}/gallery`, {
+                    method: 'POST',
+                    body: formData
+                })
+    
+                const data = await response.json()
+                const status = response.status
+    
+                console.log(status, data)
+                debugger
+            } catch(error){
+                console.log(error)
+                debugger
+            }
+
+        }
+    }
     return (
         <section className={ section }>
             <h1 className={heading}>Property Gallery</h1>
@@ -45,12 +77,23 @@ const PropertyGallery = ({ images }: {images: GalleryItem[]}) => {
                     ))
                 }
             </div>
-            <FileSelector previewBackgroundImage={updatePreviews} onFileChange={addFile}/>
+            <div className="flex gap-4">
+                <FileSelector previewBackgroundImage={updatePreviews} onFileChange={addFile}/>
+                {   
+                    Boolean(imageFiles.length) &&
+                    <Button size='large' 
+                    variant='outlined' 
+                    onClick={submitFiles}
+                    >
+                        Save Gallery
+                    </Button>
+                }
+            </div>
         </section>
     )
 }
 
 const heading = 'text-xl font-bold text-slate-800'
 const section = 'px-8 py-4 space-y-4'
-
+const API_BASE_URL = process.env.REACT_APP_API_URL
 export default PropertyGallery
